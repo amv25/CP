de9e166eacf620bc5e0c2b6e43ecad62892a409b
amv25 <77836483+amv25@users.noreply.github.com>
2022-04-16 03:10:43 +0530

    Update MinimumOfStackExtraSpace.java

--- 04 - Stack/MinimumOfStackExtraSpace.java
+++ 04 - Stack/MinimumOfStackExtraSpace.java
@@ -1,4 +1,4 @@
-//Minimun of a Stack Using Extra Space
+//Minimun of a Stack Using Extra Space - GFG
 //Link - https://practice.geeksforgeeks.org/problems/get-minimum-element-from-stack/1/
 
 import java.util.*;
